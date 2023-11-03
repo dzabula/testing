b3619571fc6053a62067b2715f1d0bd8431f909f
Marko Dasic <75902523+dzabula@users.noreply.github.com>
2023-11-03 16:00:46 +0100

    Delete MarkoDasic directory

--- MarkoDasic/Core/TokenStorage/ITokenStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MarkoDasic.Core.TokenStorage
-{
-    public interface ITokenStorage
-    {
-        void AddToken(string id);
-        bool TokenExists(string id);
-        void InvalidateToken(string id);
-    }
-}
--- MarkoDasic/DTO/LogInUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MarkoDasic.DTO
-{
-    public class LogInUserDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
